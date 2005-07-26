b0947339a275d065c1752c87dca1d8dfbd5cd58f
spike666 <spike666>
2005-07-26 02:44:01 +0000

    further implemented the conf file and everything works

--- AFX/AFX/AFXConfig.py
+++ AFX/AFX/AFXConfig.py
@@ -1,11 +1,15 @@
 import sys
 from core.AFXConf import *
 
-conf_path = "afx.conf"
+conf_path = "afx.conf" # default value...
 
 if (len(sys.argv) == 2):
 	conf_path = sys.argv[1]
 
 conf = AFXConf()
-conf.init_from_file(conf_path)
 
+try:
+	conf.init_from_file(conf_path)
+except:
+	print "a fatal error has occurred. conf file not found. AFX must close."
+	sys.exit()
--- AFX/AFX/AFXRuntime.py
+++ AFX/AFX/AFXRuntime.py
@@ -2,11 +2,11 @@ def run_module(module):
 	print "run_module(%s)\n" % module.short_name
 
 	import os
-	import AFXConf
+	from AFXConfig import conf
 
 	#my_exec = "%s/%s %s %s %s" % (AFXConf.bin_path, AFXConf.exec_bin_name, module.exec_path, module.exec_opts, module.file_name)
 
-	my_exec = "%s/%s %s" % (AFXConf.bin_path, AFXConf.exec_bin_name, module.get_exec())
+	my_exec = "%s/%s %s" % (conf.read('bin_path'), conf.read('exec_bin_name'), module.get_exec())
 
 	print "my_exec: %s" % my_exec
 
