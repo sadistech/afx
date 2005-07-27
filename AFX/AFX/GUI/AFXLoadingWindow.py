c6a1e4f3646b3f4fc98682098a74994f97fff8cf
spike666 <spike666>
2005-07-27 02:42:13 +0000

    fixed status_text so it's in the middle of the window

--- AFX/AFX/GUI/AFXLoadingWindow.py
+++ AFX/AFX/GUI/AFXLoadingWindow.py
@@ -10,18 +10,19 @@ class AFXLoadingWindow(AFXWindow):
 		self.modify_bg(gtk.STATE_NORMAL, AFX.RGBColor())
 		self.modify_fg(gtk.STATE_NORMAL, AFX.RGBColor(255, 255, 255))
 
-		self.main_box = gtk.VBox(gtk.FALSE, 0)
-		self.add(self.main_box)
-		self.main_box.show()
+		#self.main_box = gtk.VBox(gtk.FALSE, 0)
+		#self.add(self.main_box)
+		#self.main_box.show()
 
-		self.main_box.status_text = gtk.Label("Status...")
-		self.main_box.pack_start(self.main_box.status_text, gtk.FALSE, gtk.FALSE, 0)
-		self.main_box.status_text.modify_fg(gtk.STATE_NORMAL, AFX.RGBColor(255, 255, 255))
-		self.main_box.status_text.show()
+		self.status_text = gtk.Label("Status...")
+		#self.main_box.pack_start(self.main_box.status_text, gtk.FALSE, gtk.FALSE, 0)
+		self.add(self.status_text)
+		self.status_text.modify_fg(gtk.STATE_NORMAL, AFX.RGBColor(255, 255, 255))
+		self.status_text.show()
 
 
 	def update_status(self, text):
-		self.main_box.status_text.set_markup("<span size='xx-large' weight='bold'>%s</span>" % text)
-		self.main_box.status_text.queue_draw()
+		self.status_text.set_markup("<span size='xx-large' weight='bold'>%s</span>" % text)
+		self.status_text.queue_draw()
 		self.queue_draw()
 		AFX.update_ui()
