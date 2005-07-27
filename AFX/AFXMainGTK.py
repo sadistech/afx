a23a946d00987eee4ac966a19491d1fdbfcae3ec
spike666 <spike666>
2005-07-27 02:44:53 +0000

    killed some unused legacy code (old text-based menu)

--- AFX/AFXMainGTK.py
+++ AFX/AFXMainGTK.py
@@ -29,7 +29,7 @@ class App:
 		loading_window.show()
 		loading_window.update_status("Loading modules...")
 	
-		print "Loading modules..."
+		print "\nLoading modules..."
 		import Modules
 		
 		for mod in Modules.module_list:
@@ -57,53 +57,3 @@ if __name__ == "__main__":
 	app = App()
 	app.main()
 
-# NOTHING TO SEE HERE....
-
-print "Loading modules..."
-import Modules
-
-print "Loaded Modules:"
-for item in Modules.module_list:
-	print "\t%s (%s)" % (item.short_name, item.long_name)
-
-import AFX
-
-#ok, let's just loop and take commands and stuff for now...
-print "Type ? for help"
-s = ""
-while (s != 'q'):
-	s = raw_input("> ")
-	if (s == 'l'):
-		Modules.print_modules()
-	elif (s == '?'):
-		print "Commands:"
-		print "\tl: list modules"
-		print "\ti: module info"
-		print "\td: reload modules"
-		print "\tr: run module"
-		print "\t?: help"
-		print "\tq: quit\n"
-	elif (s == 'i'):
-		s = input("Module #: ")
-		Modules.print_module_info(s)
-	elif (s == "d"):
-		reload(Modules)
-	elif (s == "r"):
-		i = input("Module #: ")
-		file_name = raw_input("Filename: ")
-		Modules.get_module(i).file_name = file_name
-		AFX.run_module(Modules.get_module(i))
-	elif (s == "q"):
-		break
-	elif (s == ""):
-		# do nothing...
-		pass
-	else:
-		print "Unknown command (%s)" % s
-
-	print ""
-#Modules.module_list[2].file_name = "/home/spike/Mame\ Roms\ S-Z/sf2.zip"
-
-#AFX.run_module(Modules.module_list[2])
-
-print "ok, let's see... are we done?"
