3600d8c7f94f832ab515c88bffbcc421b62125fa
spike666 <spike666>
2005-08-14 22:11:53 +0000

    added merge() method

--- AFX/AFX/core/AFXConf.py
+++ AFX/AFX/core/AFXConf.py
@@ -1,47 +1,70 @@
 ## AFXConf.py
 ## 
 ## a wrapper for a dictionary
-## supports reading, writing and renaming keys
-## also supports initializing the dictionary from a file
-##
-## format of an AFXConf file is:
-## octothorp as the first character on a line for comments
-##	later on, maybe I should implement first non-whitespace char?
-## key_name value goes here
-## to summarize: key name is all characters up until the first whitespace char
-##	value is every character starting from the first non-whitespace character
-##	after the whitespace until the end of the line (not including the \n)
+"""
+supports reading, writing and renaming keys
+also supports initializing the dictionary from a file
 
-import re
+format of an AFXConf file is:
+octothorp as the first character on a line for comments
+later on, maybe I should implement first non-whitespace char?
+key_name value goes here
+to summarize: key name is all characters up until the first whitespace char
+	value is every character starting from the first non-whitespace character
+	after the whitespace until the end of the line (not including the \n)
+"""
+
+import re # regular expressions...
 
 class AFXConf:
 	data = {}	# create an empty dictionary...
 
 	def __init__(self):
-		# just initialize...
+		"""
+		just initialize...
+		"""
 		self.data = {}
 
 	def read(self, key):
-		# reads a key and returns the value
+		"""
+		reads a key and returns the value
+		"""
 		return self.data[key]
 
 	def write(self, key, value):
-		# sets a key to a new value.
-		# key is created if it doesn't exist
+		"""
+		sets a key to a new value.
+		key is created if it doesn't exist
+		"""
 		self.data[key] = value
 
 	def rename(self, key, new_key):
-		# renames a key (just in case)
+		""" 
+		renames a key (just in case) 
+		"""
 		self.data[new_key] = self.data[key]
 		del self.data[key]
 	
 	def keys(self):
-		# returns a list of available keys
+		"""
+		returns a list of available keys
+		"""
+		
 		return self.data.keys()
-
+	
+	def merge(self, other):
+		"""
+		merges this AFXConf with another
+		like values in other supercede values in self
+		"""
+		for k in other.keys():
+			self.write(k, other.read(k))
+		
 	def init_from_file(self, file_path):
-		# initializes the dictionary from a file
-		# see comments at top of file for conf file format
+		"""
+		initializes the dictionary from a file
+		see comments at top of file for conf file format
+		"""
 		
 		f = open(file_path, "r")
 		lines = f.readlines()
